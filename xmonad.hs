eef6521c24497a826b1facdfc5351c7f7c2de717
Alexander Soen <alexandersoen@gmail.com>
2023-10-13 11:21:06 +1100

    add submodules

--- xmonad.hs
+++ xmonad.hs
@@ -2,9 +2,17 @@ import XMonad
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Hooks.EwmhDesktops
 
 main :: IO ()
-main = xmonad $ def
+main = xmonad $ ewmhFullscreen $ ewmh myConfig
+
+myConfig = def
     {
         modMask = mod4Mask  -- Mod = Window / SuperKey
     }
+    `additionalKeysP`
+    [
+        ("M-S-z", spawn "xscreensaver-command -lock")
+    ]
+
